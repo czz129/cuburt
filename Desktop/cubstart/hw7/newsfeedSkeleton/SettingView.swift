ae6757e2e0a7ba72f82e450cc4a39643eef2aeae
Zizhuo Chen <czz129@berkeley.edu>
2022-04-15 21:15:55 -0700

    finish

---
+++ Desktop/cubstart/hw7/newsfeedSkeleton/SettingView.swift
@@ -0,0 +1,22 @@
+//
+//  SettingView.swift
+//  newsfeedSkeleton
+//
+//  Created by Zizhuo Chen on 4/15/22.
+//
+
+import Foundation
+
+import SwiftUI
+
+struct SettingsView: View {
+    var body: some View {
+        Text("Settings Screen")
+    }
+}
+
+struct SettingsView_Previews: PreviewProvider {
+    static var previews: some View {
+        SettingsView()
+    }
+}
